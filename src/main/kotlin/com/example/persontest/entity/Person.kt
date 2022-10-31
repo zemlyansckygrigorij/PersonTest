7a82258b208615eb3a762ac0a64b7f6c6bba9637
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 12:14:50 +0500

    create class Person

---
+++ src/main/kotlin/com/example/persontest/entity/Person.kt
@@ -0,0 +1,15 @@
+package com.example.persontest.entity
+
+import java.util.*
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class Person
+ */
+class Person {
+    var id: Long? = null
+    var name: String? = null
+    var birthDay: Date? =null
+    var location: String? = null
+}
