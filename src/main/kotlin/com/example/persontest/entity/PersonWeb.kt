7763ba65ed35e9e68ac723c371a956144c279cc6
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 19:38:30 +0500

    create class PersonWeb for Vaadin page

---
+++ src/main/kotlin/com/example/persontest/entity/PersonWeb.kt
@@ -0,0 +1,10 @@
+package com.example.persontest.entity
+
+import java.util.*
+
+class PersonWeb {
+    var id: Long? = null
+    var name: String? = null
+    var birthDay: String? =null
+    var location: String? = null
+}
