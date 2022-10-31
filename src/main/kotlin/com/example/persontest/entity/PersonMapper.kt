f19e3728e643cf7c45ca45eb33a772662473a62a
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 12:16:54 +0500

    create class PersonMapper for convert class Person to  PersonDao and PersonDao to Person

---
+++ src/main/kotlin/com/example/persontest/entity/PersonMapper.kt
@@ -0,0 +1,35 @@
+package com.example.persontest.entity
+
+import com.example.persontest.service.DataFormatter
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class PersonMapper
+ */
+class PersonMapper {
+    var formatter = DataFormatter()
+
+    fun getPersonFromDao(personDao: PersonDao): Person?{
+        var person = Person()
+        person.id = personDao.id
+        var birthdayStr = ""
+        var data = personDao.data
+        if (data != null) {
+            person.name = data.getValue("name") as String
+            person.location = data.getValue("location") as String
+            birthdayStr = data.getValue("birthday").toString() as String
+            person.birthDay = formatter.stringToDate(birthdayStr)
+        }else throw  Exception()
+
+        return person
+    }
+
+    fun getPersonDao(person: Person): PersonDao?{
+        val id = person.id
+        var data: Map<String, Any>
+        var dateString = formatter.dateToString(person.birthDay!!)
+        data= mapOf("name" to person.name as Any,"birthday" to dateString as Any, "location" to person.location as Any)
+        return  PersonDao(id ,data)
+    }
+}
