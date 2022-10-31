bd8666cc76f8928fbee6a4e9610353a5331d6165
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 12:05:45 +0500

    create class for convert date type

---
+++ src/main/kotlin/com/example/persontest/service/DataFormatter.kt
@@ -0,0 +1,23 @@
+package com.example.persontest.service
+
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class DataFormatter {
+
+    val formatter = SimpleDateFormat("dd/MM/yyyy")
+
+    fun dateToString( date: Date): String {
+        return formatter.format(date);
+    }
+
+    fun stringToDate(str: String) :Date{
+        try {
+            return formatter.parse(str)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return throw Exception("Wrong date format");
+    }
+}
