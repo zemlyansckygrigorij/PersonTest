95b78825d33eaea60cb10f7eb93a647cb9e12451
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 12:19:34 +0500

    update class PersonTestApplicationTests set data about developer

--- src/test/kotlin/com/example/persontest/PersonTestApplicationTests.kt
+++ src/test/kotlin/com/example/persontest/PersonTestApplicationTests.kt
@@ -3,6 +3,11 @@ package com.example.persontest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class PersonTestApplicationTests
+ */
 @SpringBootTest
 class PersonTestApplicationTests {
 
