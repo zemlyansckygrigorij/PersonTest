b5cbdc4060120a0061b580074ff8c243877e1c04
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-10-31 12:19:17 +0500

    update class PersonTestApplication set data about developer

--- src/main/kotlin/com/example/persontest/PersonTestApplication.kt
+++ src/main/kotlin/com/example/persontest/PersonTestApplication.kt
@@ -3,9 +3,13 @@ package com.example.persontest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class PersonTestApplication
+ */
 @SpringBootApplication
 class PersonTestApplication
-
 fun main(args: Array<String>) {
     runApplication<PersonTestApplication>(*args)
 }
